29ce99dd1a62f35ac963bc04b15028d3ee17c0e6
Boush Li <li.boush@gmail.com>
2014-06-10 13:46:03 +0800

    o Update.

--- src/hello.go
+++ src/hello.go
@@ -1,49 +1,10 @@
-package main
+package main 
 
-import "fmt"
-
-func Sqrt(x float64) float64 {
-        z := 1.0
-        for i := 0; i < 1000; i++ {
-                z -= (z * z - x) / (2 * z)
-        }
-        return z
-}
+import (
+	"fmt"
+)
 
 func main() {
-        var a string = "Hello"
-        var b string = "world.\n"
-        s := a + " " + b
-        fmt.Printf("Hello world.\n")
-        fmt.Printf(s)
-        for i := 0; i < 10; i++ {
-                if i > 5 {
-                        break
-                }
-                println(i)
-        }
-        list := [] int {1, 2, 3, 4, 5}
-        for k, v := range list {
-                println(k)
-                println(v)
-        }
-        j := 1
-        switch j {
-                case 0:
-                case 1:
-                        println("Hey, j is here...")
-                default:
-                        break
-        }
-        for i := 0; i < 5; i++ {
-                defer fmt.Printf("%d ", i)
-        }
-        test := func() {
-        
-        func callback(y int, f func(int)) {
-                f(y)
-        }
-                println("Anonymous function...")
-        }
-        test()
+	fmt.Printf("Hello world...\n")
 }
+
