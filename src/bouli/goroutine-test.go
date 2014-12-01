7e86aaca8bb7a89ae36424f744b8a59b21913224
Boush Li <li.boush@gmail.com>
2014-12-01 18:24:52 +0800

    o Add slice-test.go

--- src/bouli/goroutine-test.go
+++ src/bouli/goroutine-test.go
@@ -8,19 +8,21 @@ import (
 var c chan int
 
 func main() {
-	c := make(chan bool)
-	select {
-	case v := <-c:
-		fmt.Println(v)
-	case <-time.After(3 * time.Second):
-		fmt.Println("Timeout...")
-	}
+	go say("world")
+	say("Hello")
 }
 
 func ready(w string, sec int) {
 	time.Sleep(time.Duration(sec) * time.Second)
 	fmt.Println(w, "is ready")
 	c <- 1
+}
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
 }
 
 func shower(c chan int, quit chan bool) {
---
+++ src/bouli/slice-test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	p := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println("p ==", p)
+	fmt.Println("p[1: 4] ==", p[1:4])
+	fmt.Println("p[:3] ==", p[:3])
+	fmt.Println("p[4:] ==,", p[4:])
+	a := make([]int, 5)
+	fmt.Println("a ==", a)
+	b := make([]int, 0, 5)
+	fmt.Println("b ==", b)
+	printSlice("b", b)
+	c := b[:2]
+	printSlice("c", c)
+	var z []int
+	fmt.Println(z, len(z), cap(z))
+	if z == nil {
+		fmt.Println("nil!")
+	}
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
+}
