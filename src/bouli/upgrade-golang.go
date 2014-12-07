1a54df30ec68e44213a8485f6f18008fa9a7b09f
Boush Li <li.boush@gmail.com>
2014-12-07 22:39:56 +0800

    o Update.

---
+++ src/bouli/upgrade-golang.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func upgradeGolang(goSrcFile string) {
+	cmd := exec.Command("which", "go")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		_, err := out.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Golang is installed...")
+		fmt.Println("Upgrade Golang...")
+	}
+	cmd = exec.Command("sudo", "rm", "-rf", "/usr/local/go")
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("Remove Golang failed...")
+	}
+	cmd = exec.Command("sudo", "tar", "-C", "/usr/local", "zxvf", goSrcFile)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("Install Golang failed...")
+	}
+}
+
+func main() {
+	argNum := len(os.Args)
+	goSrcFile := "~/Downloads/go1.4rc2.linux-amd64.tar.gz"
+	if argNum != 2 {
+		fmt.Println("The number of arguments is not correct...")
+		fmt.Println("Default version of Golang will be installed...")
+	}
+	upgradeGolang(goSrcFile)
+}
