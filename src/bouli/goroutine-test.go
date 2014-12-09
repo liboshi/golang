893126b752e0bc39bc24aee69961e9381c3ef925
Boush Li <li.boush@gmail.com>
2014-12-09 21:11:44 +0800

    o Add test for range and close.

--- src/bouli/goroutine-test.go
+++ src/bouli/goroutine-test.go
@@ -52,32 +52,46 @@ func fibonacci(c, quit chan int) {
 	}
 }
 
+func fibonaccii(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
 func main() {
 	/*
-		go say("world")
-		say("Hello")
-		a := []int{1, 2, 3, 4, 5, 6}
-		c := make(chan int)
-		go sum(a[:len(a)/2], c)
-		go sum(a[len(a)/2:], c)
-		x, y := <-c, <-c
+			go say("world")
+			say("Hello")
+			a := []int{1, 2, 3, 4, 5, 6}
+			c := make(chan int)
+			go sum(a[:len(a)/2], c)
+			go sum(a[len(a)/2:], c)
+			x, y := <-c, <-c
 
-		fmt.Println(x, y, x+y)
-		c := make(chan int, 2)
-		c <- 1
-		c <- 2
-		fmt.Println(<-c)
-		c <- 3
-		fmt.Println(<-c)
-		fmt.Println(<-c)
-	*/
-	c := make(chan int)
-	quit := make(chan int)
-	go func() {
-		for i := 0; i < 10; i++ {
+			fmt.Println(x, y, x+y)
+			c := make(chan int, 2)
+			c <- 1
+			c <- 2
 			fmt.Println(<-c)
-		}
-		quit <- 0
-	}()
-	fibonacci(c, quit)
+			c <- 3
+			fmt.Println(<-c)
+			fmt.Println(<-c)
+		c := make(chan int)
+		quit := make(chan int)
+		go func() {
+			for i := 0; i < 10; i++ {
+				fmt.Println(<-c)
+			}
+			quit <- 0
+		}()
+		fibonacci(c, quit)
+	*/
+	c := make(chan int, 10)
+	go fibonaccii(cap(c), c)
+	for i := range c {
+		fmt.Println(i)
+	}
 }
