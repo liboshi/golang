e8a16b69f87dfe85002fdecaf763aa7185af5f1a
Boush Li <li.boush@gmail.com>
2014-12-12 15:27:28 +0800

    o First commit for new automation framework with Golang.

---
+++ src/oshi/startup.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("I will develop a new automation framework using Golang...")
+    fmt.Println("     =========         ========")
+    fmt.Println("    //       \\\\      //         \\\\")
+    fmt.Println("   ||               ||          ||")
+    fmt.Println("   ||       ===||   ||          ||")
+    fmt.Println("   ||          //   ||          ||")
+    fmt.Println("    \\\\=========      \\\\========//")
+}
