5eff6c0c401c567ee4da43e074a6abef619de186
Boush Li <li.boush@gmail.com>
2014-12-13 16:28:10 +0800

    o Update map-test.

--- src/bouli/map-test.go
+++ src/bouli/map-test.go
@@ -21,4 +21,11 @@ func main() {
 	n := make(map[int]string)
 	fmt.Println(n)
 
+	m1 := map[int]string{1: "a", 2: "b", 3: "c", 4: "d", 5: "e"}
+	fmt.Println(m1)
+	m2 := make(map[string]int)
+	for k, v := range m1 {
+		m2[v] = k
+	}
+	fmt.Println(m2)
 }
