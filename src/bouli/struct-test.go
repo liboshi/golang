4362eb4c863c3b628da4af2b22cb5f7e2e2299bb
Boush Li <li.boush@gmail.com>
2014-12-18 21:43:54 +0800

    o Update struct-test.

--- src/bouli/struct-test.go
+++ src/bouli/struct-test.go
@@ -9,7 +9,72 @@ type person struct {
 	Age  int
 }
 
+type personA struct {
+	Name    string
+	Age     int
+	Contact struct {
+		Phone, City string
+	}
+}
+
+type human struct {
+	Sex int
+}
+
+type teacher struct {
+	human
+	Name string
+	Age  int
+}
+
+type student struct {
+	human
+	Name string
+	Age  int
+}
+
+func testA(p *person) {
+	p.Age = 29
+	fmt.Println("testA", p)
+}
+
+func testB(p *person) {
+	p.Age = 30
+	fmt.Println("testB", *p)
+}
+
 func main() {
-	a := person{Name: "Li Boshi", Age: 28}
+	a := &person{Name: "Li Boshi", Age: 28}
+	b := person{}
+	b.Name = "Li Boush"
+	b.Age = 28
+	fmt.Println(*a)
+	testA(a)
 	fmt.Println(a)
+	testB(a)
+	fmt.Println(a)
+
+	c := struct {
+		Name string
+		Age  int
+	}{
+		Name: "Boush",
+		Age:  28}
+	fmt.Println(c)
+
+	d := &personA{
+		Name: "LiBoshi",
+		Age:  28,
+	}
+	d.Contact.Phone = "10086"
+	d.Contact.City = "Beijing"
+	fmt.Println(d)
+
+	e := teacher{Name: "teacher", Age: 28, human: human{Sex: 1}}
+	f := student{Name: "student", Age: 28, human: human{Sex: 0}}
+	e.Sex = 2
+	e.Name = "teacher1"
+	e.Age = 29
+	fmt.Println(e)
+	fmt.Println(f)
 }
