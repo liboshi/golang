cadf6a424d7fd316a27fe8de043332e2203cfe83
Boush Li <li.boush@gmail.com>
2014-12-18 22:00:09 +0800

    o Update method-test.

--- src/bouli/method-test.go
+++ src/bouli/method-test.go
@@ -12,6 +12,8 @@ func main() {
 	a := &A{}
 	a.Print()
 	a.SayHello("Boush")
+	fmt.Println(a.Name)
+	fmt.Println("访问控制权限由首字母大小写来控制，大写为全局，小写只能当前Package可以访问")
 }
 
 func (a *A) Print() {
@@ -20,5 +22,6 @@ func (a *A) Print() {
 }
 
 func (a *A) SayHello(name string) {
+	a.Name = "Boush"
 	fmt.Printf("Hello %s\n", name)
 }
