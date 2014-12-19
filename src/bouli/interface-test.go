8c526fbd112a58e78e6b067783caa97812abdb96
Boush Li <li.boush@gmail.com>
2014-12-19 14:17:52 +0800

    o Add one sample for interface.

--- src/bouli/interface-test.go
+++ src/bouli/interface-test.go
@@ -28,9 +28,36 @@ func Disconnect(usb USB) {
 	}
 }
 
+type Boush struct {
+	name string
+}
+
+func (b *Boush) Get() string {
+	return b.name
+}
+
+func (b *Boush) Set(name string) {
+	b.name = name
+}
+
+type Person interface {
+	Get() string
+	Set(string)
+}
+
+func Bind(p Person) {
+	fmt.Println(p.Get())
+	p.Set("Li Boush")
+	fmt.Println(p.Get())
+}
+
 func main() {
 	var a USB
 	a = PhoneConnecter{"PhoneConnecter"}
 	a.Connect()
 	Disconnect(a)
+	var p Person
+	p = &Boush{"Boush"}
+	fmt.Println(p.Get())
+	Bind(p)
 }
