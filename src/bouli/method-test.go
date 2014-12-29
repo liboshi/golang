c259af077abe25bde7d26a4114776659f800d97e
Boush Li <li.boush@gmail.com>
2014-12-29 14:49:42 +0800

    o Add answer for equlvalent binary tree.

--- src/bouli/method-test.go
+++ src/bouli/method-test.go
@@ -8,14 +8,6 @@ type A struct {
 	Name string
 }
 
-func main() {
-	a := &A{}
-	a.Print()
-	a.SayHello("Boush")
-	fmt.Println(a.Name)
-	fmt.Println("访问控制权限由首字母大小写来控制，大写为全局，小写只能当前Package可以访问")
-}
-
 func (a *A) Print() {
 	a.Name = "Li Boshi"
 	fmt.Println("A")
@@ -24,4 +16,12 @@ func (a *A) Print() {
 func (a *A) SayHello(name string) {
 	a.Name = "Boush"
 	fmt.Printf("Hello %s\n", name)
+}
+
+func main() {
+	a := &A{}
+	a.Print()
+	a.SayHello("Boush")
+	fmt.Println(a.Name)
+	fmt.Println("访问控制权限由首字母大小写来控制，大写为全局，小写只能当前Package可以访问")
 }
