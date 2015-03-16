53079b866ebb575b9eff74c1c4044cf9701a4495
Boush Li <li.boush@gmail.com>
2015-03-16 16:53:30 +0800

    o Add downloader.

--- src/oshi/startup.go
+++ src/oshi/startup.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
+
+func downloader(url, build string) {
+	out, err := os.Create("output.txt")
+	if err != nil {
+		fmt.Println("Error, Create file")
+	}
+	defer out.Close()
+	resp, err := http.Get(url + build)
+	if err != nil {
+		fmt.Println("Error, No response.")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error, read the response.")
+	}
+	fmt.Printf("%s", body)
+}
 
 func main() {
 	fmt.Println("I will develop a new automation framework using Golang...")
+	downloader("http://www.example.com/build?=", "100200")
 }
