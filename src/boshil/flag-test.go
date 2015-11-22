e6392b6020383a81fc8e354213f49c50a917cfbf
Boush Li <li.boush@gmail.com>
2015-11-22 22:41:48 +0800

    o Update test for flag. Package flag is awesome.

--- src/boshil/flag-test.go
+++ src/boshil/flag-test.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-  "fmt"
-  "flag"
+	"flag"
+	"fmt"
 )
 
 func main() {
-  duration := flag.Int("duration", 200, "Monkey test execution duration.")
-  var execPath string
-  flag.StringVar(&execPath, "execPath", "/home/bouli/exec", "Execution path user sepecified.")
-  flag.Parse()
-  fmt.Println(*duration)
-  fmt.Println(execPath)
+	duration := flag.Int("duration", 200, "Monkey test execution duration.")
+	var execPath string
+	flag.StringVar(&execPath, "execPath", "/home/boshil/test", "Execution path user sepecified.")
+	flag.Parse()
+	fmt.Println(*duration)
+	fmt.Println(execPath)
 }
