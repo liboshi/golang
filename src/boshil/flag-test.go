9dd8785a393eeefba6326cfb16cf15743dd6257b
Boush Li <li.boush@gmail.com>
2016-03-15 14:04:25 +0800

    o Update test for flag.

--- src/boshil/flag-test.go
+++ src/boshil/flag-test.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
-	duration := flag.Int("duration", 200, "Monkey test execution duration.")
 	var execPath string
+	var flagvar1 int
+	duration := flag.Int("duration", 200, "Monkey test execution duration.")
+	// Bind the flag to a variable.
+	flag.IntVar(&flagvar1, "flagname", 1234, "help message for flagname")
 	flag.StringVar(&execPath, "execPath", "/home/boshil/test", "Execution path user sepecified.")
 	flag.Parse()
 	fmt.Println(*duration)
+	fmt.Println(flagvar1)
 	fmt.Println(execPath)
 }
