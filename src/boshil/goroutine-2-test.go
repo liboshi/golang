69bfec3ab497bf6d56d508455c1385bdddd88101
Boush Li <li.boush@gmail.com>
2016-03-17 16:21:34 +0800

    o Add test for goroutine with channel.

---
+++ src/boshil/goroutine-2-test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func send_data(ch chan string) {
+	ch <- "Kevin"
+	ch <- "Frank"
+	ch <- "Vincent"
+	ch <- "Jacky"
+	ch <- "Evan"
+}
+
+func get_data(ch chan string) {
+	var input string
+	for {
+		input = <-ch
+		fmt.Printf("%s ", input)
+	}
+}
+
+func main() {
+	ch := make(chan string)
+
+	go send_data(ch)
+	go get_data(ch)
+
+	time.Sleep(1e9)
+}
