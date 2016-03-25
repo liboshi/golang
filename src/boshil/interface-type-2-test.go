957877175eabc229d65f1b93937cbe496d4d5279
Boush Li <li.boush@gmail.com>
2016-03-25 14:20:23 +0800

    o Add test for interface.

---
+++ src/boshil/interface-type-2-test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Square struct {
+	side float32
+}
+
+type Circle struct {
+	redius float32
+}
+
+type Shaper interface {
+	Area() float32
+}
+
+func (sq *Square) Area() float32 {
+	return sq.side * sq.side
+}
+
+func (ci *Circle) Area() float32 {
+	return ci.redius * ci.redius * math.Pi
+}
+
+func main() {
+	var areaIntf Shaper
+	sq1 := new(Square)
+	sq1.side = 5
+
+	areaIntf = sq1
+
+	switch t := areaIntf.(type) {
+	case *Square:
+		fmt.Printf("Type Square %T with value %v\n", t, t)
+	case *Circle:
+		fmt.Printf("Type Square %T with value %v\n", t, t)
+	case nil:
+		fmt.Println("nil value: nothing to check?")
+	default:
+		fmt.Printf("Unexpected type %T\n", t)
+	}
+}
