363437490c4873aeae8a437dffb1183061c53cd6
Boush Li <li.boush@gmail.com>
2016-03-29 15:55:01 +0800

    o Add test for reflect.

--- src/boshil/reflect-2-test.go
+++ src/boshil/reflect-2-test.go
@@ -17,4 +17,15 @@ func main() {
 	fmt.Printf("value is %5.2e\n", v.Interface())
 	y := v.Interface().(float64)
 	fmt.Println(y)
+
+	// setting a value
+	fmt.Println("settablility of v:", v.CanSet())
+	v = reflect.ValueOf(&x)
+	fmt.Println("type of v:", v.Type())
+	fmt.Println("settablility of v:", v.CanSet())
+	v = v.Elem()
+	fmt.Println("settablility of v:", v.CanSet())
+	v.SetFloat(3.1415)
+	fmt.Println(v.Interface())
+	fmt.Println(v)
 }
