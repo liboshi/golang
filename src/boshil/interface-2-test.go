fec9bfb111262dd6b0942611ef648133a393946b
Boush Li <li.boush@gmail.com>
2016-11-15 16:04:36 +0800

    o Update test for interface.

--- src/boshil/interface-2-test.go
+++ src/boshil/interface-2-test.go
@@ -4,9 +4,14 @@ import "fmt"
 
 type Shaper interface {
 	Area() float32
+	Perimeter() float32
 }
 
 type Square struct {
+	side float32
+}
+
+type SquareA struct {
 	side float32
 }
 
@@ -14,9 +19,17 @@ func (sq *Square) Area() float32 {
 	return sq.side * sq.side
 }
 
+func (sq *SquareA) Perimeter() float32 {
+	return sq.side * 4
+}
+
 func main() {
 	sq1 := new(Square)
 	sq1.side = 5
+	sq2 := new(SquareA)
+	sq2.side = 4
 	areaIntf := sq1
 	fmt.Printf("The square has area: %f\n", areaIntf.Area())
+	areaIntfA := sq2
+	fmt.Printf("The perimeter is: %f\n", areaIntfA.Perimeter())
 }
