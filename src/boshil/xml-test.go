1b10299167e22e7bfcfc92146f5727225a9838b0
Boush Li <li.boush@gmail.com>
2016-11-28 17:39:11 +0800

    o Update test for xml.

--- src/boshil/xml-test.go
+++ src/boshil/xml-test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -22,10 +23,23 @@ func main() {
 		Comment string `xml:",comment"`
 	}
 
+	type RecurlyPerson struct {
+		XMLName   xml.Name `xml:"person"`
+		Id        int      `xml:"id,attr"`
+		FirstName string   `xml:"name>first"`
+		LastName  string   `xml:"name>last"`
+		Age       int      `xml:"age"`
+		Height    float32  `xml:"height,omitempty"`
+		Married   bool
+		Address
+		Comment string `xml:",comment"`
+	}
+
 	f, err := os.OpenFile("output.xml", os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
 		fmt.Println(err)
 	}
+	defer f.Close()
 
 	v := &Person{Id: 13, FirstName: "John", LastName: "Doe", Age: 42}
 	v.Comment = " Need more details. "
@@ -36,4 +50,21 @@ func main() {
 	if err := enc.Encode(v); err != nil {
 		fmt.Printf("error: %v\n", err)
 	}
+
+	// Read the content in xml
+	file, errr := os.Open("output.xml")
+	defer file.Close()
+	data, errr := ioutil.ReadAll(file)
+	fmt.Printf("%s\n", data)
+	if errr != nil {
+		fmt.Printf("error: %v", errr)
+		return
+	}
+	vr := RecurlyPerson{}
+	errr = xml.Unmarshal(data, &vr)
+	if errr != nil {
+		fmt.Printf("error: %v:", errr)
+		return
+	}
+	fmt.Println(vr)
 }
